308bd3b89fad6bdae4f6904927cbe5364a459a25
engspark <josh@magicmyth.com>
2021-05-12 00:42:22 +0100

    Basic serial commmunications working
    Basic program now puts out the word 'hello' as a string through the
    serial port connected to the USB port. Testing conducted at low baud
    rate of 150 to visually confirm correct operation.

--- pulsetransmit.cpp
+++ pulsetransmit.cpp
@@ -6,33 +6,41 @@
 #include <wiringPi.h>
 #include <wiringSerial.h>
 
+
 int main ()
 {
   int serial_port ;
-  char dat;
+  char dat[6] = "hello";
+  
+  
   
   // open serial port
-  if ((serial_port = serialOpen ("/dev/ttyS0", 300)) < 0)	
+  if ((serial_port = serialOpen("/dev/ttyUSB0", 150)) < 0)	
   {
     fprintf (stderr, "Unable to open serial device: %s\n", strerror (errno)) ;
     return 1 ;
   }
-
-  if (wiringPiSetup () == -1)					/* initializes wiringPi setup */
+  // initializes wiringPi setup
+  if (wiringPiSetup () == -1)					
   {
     fprintf (stdout, "Unable to start wiringPi: %s\n", strerror (errno)) ;
     return 1 ;
   }
 
-  while(1){
+  while(1)
+  {
 	  
-	if(serialDataAvail (serial_port) )
-	{ 
-		dat = serialGetchar (serial_port);		/* receive character serially*/	
-		printf ("%c", dat) ;
-		fflush (stdout) ;
-		serialPutchar(serial_port, dat);		/* transmit character serially on port */
-		  }
-	}
+	//if(serialDataAvail (serial_port) )
+	//{ 
+	  // receive character serially
+	  //dat = serialGetchar (serial_port);			
+	  printf ("%s", dat) ;
+
+	  //fflush (stdout) ;
+	  // transmit character serially on port
+	  serialPrintf(serial_port, dat);		
+	//}
+	delay(100);
+  }
 
 }

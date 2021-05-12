0dc24b80a83f47d840639a2d532ddc851356c61d
engspark <josh@magicmyth.com>
2021-05-12 18:52:28 +0100

    Dual Serial ports running, one for TX, one for RX
    Now have basic dual serial ports open and running on the pi 3B+, Tx can
    run at any speed from 150 baud to 230400 baud. Rx however seems to be
    only able to run at a minimum speed of 9600 baud, will investigate
    further through a series of tests.

--- pulsetransmit.cpp
+++ pulsetransmit.cpp
@@ -10,23 +10,28 @@
 int main ()
 {
   int serial_port ;
+  int serial_port1 ;
   char dat[6] = "hello";
+  char dat1[8] = "goodbye";
   
-  
-  
+
   // open serial port
-  if ((serial_port = serialOpen("/dev/ttyUSB0", 150)) < 0)	
+  /*if ((serial_port = serialOpen("/dev/ttyS0", 150)) < 0)	
   {
+    
     fprintf (stderr, "Unable to open serial device: %s\n", strerror (errno)) ;
     return 1 ;
   }
+  */
   // initializes wiringPi setup
   if (wiringPiSetup () == -1)					
   {
     fprintf (stdout, "Unable to start wiringPi: %s\n", strerror (errno)) ;
     return 1 ;
   }
-
+  serial_port1 = serialOpen("/dev/ttyUSB0", 230400);
+  serial_port = serialOpen("/dev/ttyS0", 9600);
+  
   while(1)
   {
 	  
@@ -34,13 +39,17 @@ int main ()
 	//{ 
 	  // receive character serially
 	  //dat = serialGetchar (serial_port);			
-	  printf ("%s", dat) ;
-
-	  //fflush (stdout) ;
+	  //printf ("%s", dat) ;
+	  serialPrintf(serial_port1, dat1);
+	  fflush (stdout) ;
+	  serialPrintf(serial_port, dat);
+	  fflush (stdout) ;
+	  delay (5);
 	  // transmit character serially on port
-	  serialPrintf(serial_port, dat);		
+
+	  		
 	//}
-	delay(100);
+	//delay(1);
   }
 
 }
